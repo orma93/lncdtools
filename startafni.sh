45b7535a89aa63656582521feac9792dc40e7679
WillForan <will.foran@gmail.com>
2016-05-18 13:21:04 -0400

    add file check to suma, specify #!shabang

--- startafni.sh
+++ startafni.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 export t1image=${HOME}/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_brain.nii
 export t1image23=${HOME}/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_brain_2.3mm.nii
 
--- startsuma.sh
+++ startsuma.sh
@@ -1,6 +1,13 @@
+#!/usr/bin/env bash
+
 export specFile=${HOME}/standard/suma_mni/N27_both.spec
 export t1image=${HOME}/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_brain.nii
 export t1image23=${HOME}/standard/mni_icbm152_nlin_asym_09c/mni_icbm152_t1_tal_nlin_asym_09c_brain_2.3mm.nii
+
+# check all these files
+for f in  specFile t1image23 t1image; do
+ [ ! -r "${!f}" ]  && echo "missing $f: ${!f}! copy from /opt/ni_tools/standard_templates/" >&2 && exit 1
+done
 
 afni -niml -yesplugouts -com "SET_UNDERLAY $t1image" -dset $t1image $t1image23 &
 suma -niml -spec $specFile -sv $t1image &
